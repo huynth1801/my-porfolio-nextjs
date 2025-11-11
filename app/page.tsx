1a5ec7ade703d2c33110657512412080eeba0a0a
huynth1801 <huuhuy1801@gmail.com>
2025-11-11 21:53:19 +0700

    add skill section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import AboutSection from "./components/about-section";
 import ExperienceSection from "./components/experience-section";
+import OrbitingSkills from "./components/skill";
 
 export default function Home() {
   return (
     <main className="bg-[#0d0d0f]">
       <AboutSection />
       <ExperienceSection />
+      <OrbitingSkills />
     </main>
   );
 }
