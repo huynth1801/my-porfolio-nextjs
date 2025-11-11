66156767f79108e39d45f8a45737e1141bbef201
huynth1801 <huuhuy1801@gmail.com>
2025-11-11 21:33:20 +0700

    add experience section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import AboutSection from "./components/about-section";
+import ExperienceSection from "./components/experience-section";
 
 export default function Home() {
   return (
     <main className="bg-[#0d0d0f]">
       <AboutSection />
+      <ExperienceSection />
     </main>
   );
 }
